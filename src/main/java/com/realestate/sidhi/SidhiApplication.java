be390bf72dd4b054911e4bb8a02cf9bb8bcbb8e8
AnandManit09 <anand8manit@gmail.com>
2025-01-14 17:47:37 +0530

    Boilerplate code from Spring initializer

---
+++ src/main/java/com/realestate/sidhi/SidhiApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.sidhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidhiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidhiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realestate/sidhi/SidhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.sidhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidhiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
